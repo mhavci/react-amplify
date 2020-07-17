de76d20ee2526d70e79be72684a2506d3dbec983
Huzeyfe <huzeyfeavci@hotmail.com>
2020-07-17 20:34:19 +0300

    graphql

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import { Auth, API, graphqlOperation } from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifyGreetings } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+import * as queries from './graphql/queries';
+Auth.configure(awsconfig);
+API.configure(awsconfig);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React-Amplify
-        </a>
-      </header>
-    </div>
+
+const AuthStateApp = () => {
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+
+
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+      <div className="App">
+        <AmplifyGreetings username={user.username}></AmplifyGreetings>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    ) : (
+      <AmplifyAuthenticator />
   );
 }
 
-export default App;
+export default AuthStateApp;
